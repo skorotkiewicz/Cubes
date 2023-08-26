7257da85d3e0553b9c944b65460fea85c5369c5d
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2023-08-26 02:57:00 +0200

    hm4

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,10 @@ WORKDIR /app
 COPY package.json .
 
 RUN npm install
-RUN npm run build
 
 COPY . .
+
+RUN npm run build
 
 ENV PORT=5000
 
--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-  },
 });
